6f150050dd5842ea62eae1532b9dd24375e92bb2
reyyuvraj <yuvrajsingh77733999@gmail.com>
2022-08-29 17:12:41 +0530

    app icon

--- app/src/main/java/com/example/notescompose/featurenote/presentation/addeditnote/AddEditNoteScreen.kt
+++ app/src/main/java/com/example/notescompose/featurenote/presentation/addeditnote/AddEditNoteScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Save
@@ -130,7 +131,7 @@ fun AddEditNoteScreen(
                 },
                 isHintVisible = titleState.isHintVisible,
                 singleLine = true,
-                textStyle = MaterialTheme.typography.h5
+                textStyle = MaterialTheme.typography.h5,
             )
             Spacer(modifier = Modifier.height(16.dp))
             TransparentHintTextField(
--- app/src/main/java/com/example/notescompose/featurenote/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/example/notescompose/featurenote/presentation/notes/NotesScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -14,6 +15,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
